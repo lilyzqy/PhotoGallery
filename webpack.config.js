93b21b0edfe308b6872501200fbb79d9b63c32e9
Lily Yang <zq.yang.lily@gmail.com>
2018-04-06 17:12:45 -0700

    handle latest javascrept/ES6 code with extensions

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
-  module.exports = {
-    entry: './src/entry.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    }
-  };
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
