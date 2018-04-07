a507ae06324a2c260d48cf781da974669c61f129
Lily Yang <zq.yang.lily@gmail.com>
2018-04-07 11:14:56 -0700

    images returning a output path

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,3 +1,4 @@
 import './js/test1.js';
 import './js/test2.js';
 import './scss/basic.scss';
+import './scss/more.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,13 @@ module.exports = {
                 }
             }]
         })
-      }
+      },
+      {
+       test: /\.(png|svg|jpg|gif)$/,
+       use: [
+         'file-loader'
+       ]
+     }
     ]
   },
   devtool: 'source-map',
