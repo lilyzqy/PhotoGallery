fe0fca62cbf51acdf2359936aa34e7ad636b70dc
Lily Yang <zq.yang.lily@gmail.com>
2018-04-08 16:22:33 -0700

    moved photos to util

--- dist/bundle.js
+++ dist/bundle.js
@@ -106,6 +106,8 @@ var _react = __webpack_require__(13);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _photos = __webpack_require__(33);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -124,42 +126,9 @@ var App = function (_React$Component) {
   }
 
   _createClass(App, [{
-    key: "render",
+    key: 'render',
     value: function render() {
-      // image items will be located in store in real apps
-      var items = [{ url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228559/fushimi-inari-taisha-japan-temple-96420_e5vr4m.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/architecture-contemporary-daylight-210547_xkxfpf.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/asia-japan-japanese-590478_ygvhi6.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/branch-cherry-blossom-environment-356269_fyiqhn.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/ancient-architecture-asia-301614_jlcmuc.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/architecture-asia-asian-950834_mztdnn.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228557/ancient-art-asia-302100_yly2al.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228556/architecture-building-cold-707677_ejpmlf.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228555/appetizer-cooking-cuisine-461271_ep8sgh.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228555/animal-close-up-hairy-40653_btayz5.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }, {
-        url: "http://res.cloudinary.com/dreamhousesf/image/upload/v1523228556/beautiful-bloom-blossom-928971_uqpkbk.jpg",
-        caption: "Senbon Torii/ Kyoto"
-      }];
+      var items = _photos.photos;
     }
   }]);
 
@@ -19548,6 +19517,12 @@ function camelize(string) {
 }
 
 module.exports = camelize;
+
+/***/ }),
+/* 33 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/Users/guangliangchen/Documents/JOB_SEARCH/HotPads/my_gallery/src/util/photos.js'\n    at Error (native)");
 
 /***/ })
 /******/ ]);
---
+++ src/util/photos.js
@@ -0,0 +1,34 @@
+export const photos = [{url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228559/fushimi-inari-taisha-japan-temple-96420_e5vr4m.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/architecture-contemporary-daylight-210547_xkxfpf.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/asia-japan-japanese-590478_ygvhi6.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/branch-cherry-blossom-environment-356269_fyiqhn.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/ancient-architecture-asia-301614_jlcmuc.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/architecture-asia-asian-950834_mztdnn.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228557/ancient-art-asia-302100_yly2al.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228556/architecture-building-cold-707677_ejpmlf.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228555/appetizer-cooking-cuisine-461271_ep8sgh.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228555/animal-close-up-hairy-40653_btayz5.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              },{
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228556/beautiful-bloom-blossom-928971_uqpkbk.jpg",
+                caption:"Senbon Torii/ Kyoto"
+              }
+];
