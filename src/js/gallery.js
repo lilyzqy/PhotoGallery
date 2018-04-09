e6a192f0ace9ad11e1ac3ddfaf83b3da87ab4435
Lily Yang <zq.yang.lily@gmail.com>
2018-04-08 21:10:01 -0700

    slide showing

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,20 +1,35 @@
 import React from 'react';
 
-class App extends React.Component{
+class Gallery extends React.Component{
   constructor(){
     super();
+    this.state= {
+      currentIndex: 0,
+    };
   }
 
   render(){
+    const { items }= this.props;
+    let slides= [];
+    let thumbnails= [];
+    items.forEach((item,index)=>{
+      const selectedClass= this.state.currentIndex === index ? "selected" : "";
+      const photo = this.state.currentIndex === index ? <img src={item.url}></img> : <div></div>;
+      const slide = (<div className={"slide "+ selectedClass}>
+                       {photo}
+                     </div>);
+      slides.push(slide);
+    });
     return(
       <div className="gallery-wrapper">
-        <section className="gallery-upper-slide">
+        <section className="gallery-upper-slide-warpper">
+          {slides}
         </section>
-        <section className="gallery-lower-thumbnails">
+        <section className="gallery-lower-thumbnails-wrapper">
         </section>
       </div>
     );
   }
 }
 
-export default App;
+export default Gallery;
