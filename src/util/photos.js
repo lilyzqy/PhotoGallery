a96ed721b3518c92686fd4b4a79c5ed103403a81
Lily Yang <zq.yang.lily@gmail.com>
2018-04-09 01:46:29 -0700

    updated photos

--- src/util/photos.js
+++ src/util/photos.js
@@ -1,34 +1,34 @@
-export const photos = [{url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228559/fushimi-inari-taisha-japan-temple-96420_e5vr4m.jpg",
-                caption:"Senbon Torii/ Kyoto"
+export const photos = [{url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523259040/Shirahige_Shrine_Takashima_pk9jtm.jpg",
+                caption:"Shirahige Shrine, Takashima"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/architecture-contemporary-daylight-210547_xkxfpf.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523259040/Shibuya_Crossing_Tokyo_cqumr8.jpg",
+                caption:"Shibuya Crossing, Tokyo"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/asia-japan-japanese-590478_ygvhi6.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523259040/Osaka_Castle_Osaka_fhlkn6.jpg",
+                caption:"Osaka Castle, Osaka"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/branch-cherry-blossom-environment-356269_fyiqhn.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523259039/Meiji_Shrine_Inner_Garden_Shibuya_spdbjx.jpg",
+                caption:"Meiji Shrine Inner Garden, Shibuya"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/ancient-architecture-asia-301614_jlcmuc.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523263393/ancient-art-asia-302100_2_msdg26.jpg",
+                caption:"Lake Toya, Hokkaido"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228558/architecture-asia-asian-950834_mztdnn.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523259038/Fushimi_Inari-taisha_Kyoto_mkfyzp.jpg",
+                caption:"Senbon Torii, Kyoto"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228557/ancient-art-asia-302100_yly2al.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523259037/Hirosaki_Castle_Hirosaki_ekxujm.jpg",
+                caption:"Hirosaki Castle, Hirosaki"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228556/architecture-building-cold-707677_ejpmlf.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523259039/Fuji_Moutain_Tokyo_hnupde.jpg",
+                caption:"Fuji Moutain, Tokyo"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228555/appetizer-cooking-cuisine-461271_ep8sgh.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523263204/redd-angelo-370362-unsplash_2_ydampi.jpg",
+                caption:"Yasaka Shrine, Kyoto"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228555/animal-close-up-hairy-40653_btayz5.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523259039/Kinkaku-ji_Kyoto_bcqwrc.jpg",
+                caption:"Kinkaku-ji, Kyoto"
               },{
-                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523228556/beautiful-bloom-blossom-928971_uqpkbk.jpg",
-                caption:"Senbon Torii/ Kyoto"
+                url:"http://res.cloudinary.com/dreamhousesf/image/upload/v1523263478/architecture-asia-asian-950834_2_sg1flu.jpg",
+                caption:"Sensō-ji, Tokyo"
               }
 ];
